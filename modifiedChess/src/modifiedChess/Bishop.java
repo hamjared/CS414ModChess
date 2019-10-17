e33e8f44005dea949f8f79ac4e972c5d51f6485d
Jared <jaredlham@gmail.com>
2019-10-17 11:14:48 -0600

    bishop legal moves done

--- modifiedChess/src/modifiedChess/Bishop.java
+++ modifiedChess/src/modifiedChess/Bishop.java
@@ -2,7 +2,11 @@ package modifiedChess;
 
 import java.util.ArrayList;
 
+
+
 public class Bishop extends ChessPiece {
+	
+	private enum Direction {UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT};
 
 	public Bishop(ChessBoard board, Color color) {
 		super(board, color);
@@ -21,7 +25,88 @@ public class Bishop extends ChessPiece {
 	@Override
 	public ArrayList<String> legalMoves() {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> legalMovesArray = new ArrayList<>();
+		
+		this.move(legalMovesArray, Direction.UPLEFT);
+		this.move(legalMovesArray, Direction.UPRIGHT);
+		this.move(legalMovesArray, Direction.DOWNLEFT);
+		this.move(legalMovesArray, Direction.DOWNRIGHT);
+		
+		return legalMovesArray;
+	}
+	
+	private void move(ArrayList<String> legalMoveArray, Direction direction) {
+		int rowIncrement = 0;
+		int rowChange = 0;
+		int colIncrement = 0;
+		int colChange = 0;
+		
+		switch (direction) {
+		case UPLEFT:
+			rowIncrement = 1;
+			rowChange = 1;
+			colIncrement = -1;
+			colChange = -1;
+			break;
+			
+		case DOWNLEFT:
+			rowIncrement = -1;
+			rowChange = -1;
+			colIncrement = -1;
+			colChange = -1;
+			break;
+		case UPRIGHT:
+			rowIncrement = 1;
+			rowChange = 1;
+			colIncrement = 1;
+			colChange = 1;
+			break;
+		case DOWNRIGHT:
+			rowIncrement = -1;
+			rowChange = -1;
+			colIncrement = 1;
+			colChange = 1;
+			break;
+		}
+		
+		
+		while(true) {
+			try {
+				String newPosition = this.newPositionStringForm(rowIncrement, colIncrement);
+				if (this.board.getPiece(newPosition) != null) {
+					if(this.checkCapture(newPosition)) {
+						legalMoveArray.add(newPosition);
+					}
+					break;
+				}
+				legalMoveArray.add(newPosition);
+			} catch (IllegalPositionException e) {
+				// TODO Auto-generated catch block
+				break;
+			}
+			rowIncrement += rowChange;
+			colIncrement += colChange;
+		}
+	}
+	
+	
+	 
+	private void validatePosition(String position) throws IllegalPositionException {
+		if(!position.matches("[a-h][1-8]"))
+			throw new IllegalPositionException();
+		
+	}
+	
+	private String newPositionStringForm(int incrementRow, int incrementColumn) throws IllegalPositionException {
+		int newRow = row + incrementRow;
+		int newColumn = column + incrementColumn;
+		String newPosition = "" + (char)(newColumn + 'a') + (char)(newRow + '1');
+		this.validatePosition(newPosition);
+		return newPosition;
+	}
+	
+	private boolean checkCapture(String newPosition) throws IllegalPositionException {
+		return this.board.getPiece(newPosition).getColor() != this.getColor();
 	}
 
 }
--- modifiedChess/src/modifiedChess/BishopTest.java
+++ modifiedChess/src/modifiedChess/BishopTest.java
@@ -2,10 +2,15 @@ package modifiedChess;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.junit.Before;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import modifiedChess.ChessPiece.Color;
 
 class BishopTest {
 	
@@ -41,13 +46,78 @@ class BishopTest {
 	}
 
 	@Test
-	void testLegalMoves() {
-		fail("Not yet implemented");
+	void testLegalMoves1() throws IllegalPositionException {
+		//check all bishops in board starting state have no valid moves. 
+		ChessBoard board = new ChessBoard();
+		board.initialize();
+		String[] bishopLocations = {"c1", "c8", "f8", "f1"};
+		for(int i = 0 ; i < bishopLocations.length; i++) {
+			ChessPiece bishop = board.getPiece(bishopLocations[i]);
+			assert(bishop instanceof Bishop); //double check
+			assert(bishop.legalMoves().isEmpty());
+			
+		}
+		
+		
+		
 	}
-
+	
 	@Test
-	void testBishop() {
-		fail("Not yet implemented");
+	void testLegalMoves2() throws IllegalPositionException {
+		//place white bishop at e4, with no other pieces on board. Expected moves are:
+		// d5 c6 b7 a8 f3 g2 h1 d3 c2 b1 f5 g6 h7
+		ChessBoard board = new ChessBoard();
+		Bishop bishop = new Bishop(board, Color.White);
+		board.placePiece(bishop, "e4");
+		String[] expectedArray = {"d5","c6","b7","a8","f3", "g2", "h1", "d3", "c2", "b1", "f5", "g6", "h7"};
+		ArrayList<String> expected = new ArrayList<>();
+		Collections.addAll(expected, expectedArray);
+		
+		assertTrue(bishop.legalMoves().containsAll(expected));
+		assertTrue(bishop.legalMoves().size() == expected.size());
+		
 	}
+	
+	@Test
+	void testLegalMoves3() throws IllegalPositionException {
+		//place black bishop at e4, with no other pieces on board. Expected moves are:
+		// d5 c6 b7 a8 f3 g2 h1 d3 c2 b1 f5 g6 h7
+		ChessBoard board = new ChessBoard();
+		Bishop bishop = new Bishop(board, Color.Black);
+		board.placePiece(bishop, "e4");
+		String[] expectedArray = {"d5","c6","b7","a8","f3", "g2", "h1", "d3", "c2", "b1", "f5", "g6", "h7"};
+		ArrayList<String> expected = new ArrayList<>();
+		Collections.addAll(expected, expectedArray);
+		
+		assertTrue(bishop.legalMoves().containsAll(expected));
+		assertTrue(bishop.legalMoves().size() == expected.size());
+		
+	}
+	
+	@Test
+	void testLegalMoves4() throws IllegalPositionException {
+		//place black bishop at e4, with white piece at c6, white piece at c2  black piece at g2 . Expected moves are:
+		// d5 c6 d3 c2 f3 f5 g6 h7
+		ChessBoard board = new ChessBoard();
+		Bishop bishop = new Bishop(board, Color.Black);
+		board.placePiece(bishop, "e4");
+		Knight knight = new Knight(board, Color.White);
+		board.placePiece(knight, "c6");
+		Queen queen = new Queen(board, Color.White);
+		board.placePiece(queen, "c2");
+		Rook rook = new Rook(board, Color.Black);
+		board.placePiece(rook, "g2");
+		
+		String[] expectedArray = {"d5","c6","d3","c2","f3", "f5", "g6", "h7"};
+		ArrayList<String> expected = new ArrayList<>();
+		Collections.addAll(expected, expectedArray);
+		
+		assertTrue(bishop.legalMoves().containsAll(expected));
+		assertTrue(bishop.legalMoves().size() == expected.size());
+		
+	}
+	
+
+
 
 }
